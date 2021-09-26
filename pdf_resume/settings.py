317554d4fd67c9ceeedf279b63cea50266082055
Sanket Pande <sanketpande99001@gmail.com>
2021-09-26 21:04:10 +0530

    added allowed hosts

--- pdf_resume/settings.py
+++ pdf_resume/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-zc%gl%uvx!yo^c9933!q)=^+(_64o)y*3^(wjk*9+$w(hmr@(s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['warm-plateau-09414.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
